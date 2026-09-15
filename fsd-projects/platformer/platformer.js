cc9efffada13dfd1c7e29ad75c6478c789bc284a
AndrewJM-pop <moeandjoe13@gmail.com>
2026-09-15 14:45:20 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -26,24 +26,33 @@ $(function () {
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
+    
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-
-
+      createPlatform(400,620,250,180,"black");
+      createPlatform(250,500,120,25,"black");
+      createPlatform(520,380,80,25,"black");
+      createPlatform(700,300,10,100,"black");
+      createPlatform(700,300,150,10,"black");
+      createPlatform(850,200,10,110,"black");
+      createPlatform(850,200,150,10,"black");
+      createPlatform(1200,200,40,30,"black");
+      createPlatform(800,500,40,30,"black");
 
     // TODO 3 - Create Collectables
 
-
-
+      createCollectable("diamond", 800, 450);
+      createCollectable("diamond", 1200, 150);
+      createCollectable("diamond", 850, 150);
     
     // TODO 4 - Create Cannons
 
-
-    
+      createCannon("bottom", 700, 1000);
+      createCannon("top", 700, 1250);
+      createCannon("right", 150, 1500);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
